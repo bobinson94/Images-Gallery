65de7a5f431741b524cf9452724b3249025a1d8b
bobinson94 <bobinson94@gmail.com>
2022-11-25 19:47:40 -0700

    First API Call

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
-
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 
 const App = () => {
@@ -13,8 +13,18 @@ const App = () => {
   const handleSearchSubmit = (e) => {
    // prevents the app from reloading once submit is clicked 
   e.preventDefault();
-  console.log(e.target[0].value);
+  console.log(word);
+  fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    })
+    // catches any error that may occur while fetching the api
+    .catch((err) => {
+      console.log(err);
+    })
 }
+
 
 
   return (
