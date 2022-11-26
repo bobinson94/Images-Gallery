5457d6bb1b0e0317e64ff1d9d796382212dc28e1
bobinson94 <bobinson94@gmail.com>
2022-11-25 17:57:03 -0700

    controlled searched component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Search from './components/Search';
 
-function App() {
+
+
+
+const App = () => {
+  //useState to make this a controlled input
+  const [word, setWord] = useState('');
+
+  const handleSearchSubmit = (e) => {
+   // prevents the app from reloading once submit is clicked 
+  e.preventDefault();
+  console.log(e.target[0].value);
+}
+
+
   return (
     <div className="App">
-      < Header />
+      < Header title="Images Gallery" />
+      < Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
