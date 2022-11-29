a1269e0d91b86811b8419b2c70cd333b81097470
bobinson94 <bobinson94@gmail.com>
2022-11-28 17:42:42 -0700

    Fixed all the issues using lint

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { Container, Row, Col, Button, Form } from "react-bootstrap";
 
-
-
-const Search = ({  word, setWord, handleSubmit}) => {
+const Search = ({ word, setWord, handleSubmit }) => {
   return (
     <Container className="mt-4">
       <Row className="justify-content-center ">
@@ -11,18 +9,19 @@ const Search = ({  word, setWord, handleSubmit}) => {
           <Form onSubmit={handleSubmit}>
             <Row>
               <Col xs={9}>
-                <Form.Control 
-                    type="text"
-                    // With the porps word and set word we place them here to make this a controlled input
-                    value={word}
-                    // updates with help of setWord the value stored inside of app component
-                    onChange={(e) => setWord(e.target.value)}
-                    placeholder="Search for new image here:" 
-
+                <Form.Control
+                  type="text"
+                  // With the porps word and set word we place them here to make this a controlled input
+                  value={word}
+                  // updates with help of setWord the value stored inside of app component
+                  onChange={(e) => setWord(e.target.value)}
+                  placeholder="Search for new image here:"
                 />
               </Col>
               <Col>
-                <Button variant="primary" type="submit">Search</Button>
+                <Button variant="primary" type="submit">
+                  Search
+                </Button>
               </Col>
             </Row>
           </Form>
