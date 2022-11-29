db5b9915bcecbcd7668843e44547a1c275d23ea3
bobinson94 <bobinson94@gmail.com>
2022-11-28 17:33:00 -0700

    eslint and prettier setup

--- src/App.js
+++ src/App.js
@@ -1,40 +1,39 @@
-import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Search from './components/Search';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
-
 const App = () => {
   //useState to make this a controlled input
-  const [word, setWord] = useState('');
+  const [word, setWord] = useState("");
 
   const handleSearchSubmit = (e) => {
-   // prevents the app from reloading once submit is clicked 
-  e.preventDefault();
-  console.log(word);
-  fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    })
-    // catches any error that may occur while fetching the api
-    .catch((err) => {
-      console.log(err);
-    })
+    // prevents the app from reloading once submit is clicked
+    e.preventDefault();
+    console.log(word);
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      // catches any error that may occur while fetching the api
+      .catch((err) => {
+        console.log(err);
+      });
 
-    setWord('');
-}
-
-
+    setWord("");
+  };
 
   return (
     <div className="App">
-      < Header title="Images Gallery" />
-      < Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
-}
+};
 
 export default App;
