11563c679b7351505a58b060f062f4e907282f18
bobinson94 <bobinson94@gmail.com>
2022-12-07 19:51:04 -0700

    Welcome Component

---
+++ src/components/Welcome.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+
+const Welcome = () => {
+  return (
+    <Card class="jumbotron m-3">
+      <Card.Body>
+        <Card.Title class="display-4">Images Gallery</Card.Title>
+        <Card.Text class="lead">
+          This is a simple application that retrieves photos using Unsplash API.
+          In order to start, enter any search term in the imput field.
+        </Card.Text>
+        <Button variant="primary" href="https://unsplash.com" target="_blank">
+          Learn More
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Welcome;
